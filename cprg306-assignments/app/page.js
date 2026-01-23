8da6e617e7cbba034a9af6cafd4c753b390b1a9e
donjoshua-anil <donjoshuaanil2005@gmail.com>
2026-01-23 13:12:52 -0700

    Version 0.1

---
+++ cprg306-assignments/app/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>
+        CPRG 306:Web Development 2 - Assignments
+      </h1>
+      <Link href= "/week2">week-2 </Link>
+      <p>By Don Joshua Anil</p>
+    </main>
+  );
+}
---
+++ cprg306-assignments/app/week2/StudentInfo.js
@@ -0,0 +1,15 @@
+import link from "next/link";
+
+export default function StudentInfo(){
+    return(
+        <section>
+            <h2>
+                Don JOshua ANil
+            </h2>
+            <p>
+                <link>
+                </link>
+            </p>
+        </section>
+    );
+}
